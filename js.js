1cc3b6950c58e6bb14753b578f0c83f1578b158f
SpicyNine <148865918+SpicyNine@users.noreply.github.com>
2023-10-27 01:58:11 +0100

    Started toggle logic for filtering seasonal activities

--- js.js
+++ js.js
@@ -1,1 +1,8 @@
-console.log('hw');
+function tglSeasonBtn(season){
+    // set a "selected" and a default "unselected" class
+    document.getElementById(season + '-title').style.fontWeight = 'bold';
+    document.querySelector('.' + season + '-div').style.border = '1px solid black';
+
+    console.log(document.querySelector('.' + season + '-div'));
+    console.log('test332');
+}
